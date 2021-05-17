8bcfccf45485273a9d3d954279028f91e2be0700
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-17 21:55:29 +0000

    Added techhub/profiles's backend get and post requests 		# Separate subject from body with a blank line 		# Limit the subject line to 50 characters 		# Capitalize the subject line 		# Do not end the subject line with a period 		# Use the imperative mood in the subject line 		# Wrap the body at 72 characters 		# Use the body to explain what and why vs. how 	# Note: delete this prompt when you write your message

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,15 @@
 import Router from 'koa-router'
 
 import publicRouter from './public.js'
-import secureRouter from'./custhub.js'
+import custhub from'./custhub.js'
 import reportIssue from'./reportIssue.js'
 import relevantTechnicians from './relevantTechs.js'
 import techhub from './techhub.js'
+import techProfile from './techProfile.js'
 
 const mainRouter = new Router()
 
-const nestedRoutes = [publicRouter, secureRouter, reportIssue, relevantTechnicians, techhub]
+const nestedRoutes = [publicRouter, custhub, reportIssue, relevantTechnicians, techhub, techProfile]
 for (const router of nestedRoutes) {
 	mainRouter.use(router.routes())
 	mainRouter.use(router.allowedMethods())
