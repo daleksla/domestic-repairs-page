ddd31881c0c9d0968b878e2aeb39951c853ebae9
Salih Ahmed <ahmed233@coventry.ac.uk>
2021-05-17 21:50:59 +0000

    Added unit tests
    Added unit testing for record-updating functions
    		# Separate subject from body with a blank line
    		# Limit the subject line to 50 characters
    		# Capitalize the subject line
    		# Do not end the subject line with a period
    		# Use the imperative mood in the subject line
    		# Wrap the body at 72 characters
    		# Use the body to explain what and why vs. how
    	# Note: delete this prompt when you write your message

--- unitTests/test-technicianDetails.js
+++ unitTests/test-technicianDetails.js
@@ -19,6 +19,7 @@ test('REGISTER : error if blank techID', async test => {
 		test.is(err.message, 'missing field', 'incorrect error message')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
 
@@ -37,6 +38,7 @@ test('REGISTER : error if blank phone number', async test => {
 		test.is(err.message, 'missing field', 'incorrect error message')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
 
@@ -53,9 +55,9 @@ test('REGISTER : error if blank types', async test => {
 		test.is(err.message, 'missing field', 'incorrect error message')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
-
 
 test('REGISTER : error if blank address', async test => {
 	test.plan(1)
@@ -72,6 +74,27 @@ test('REGISTER : error if blank address', async test => {
 		test.is(err.message, 'missing field', 'incorrect error message')
 	} finally {
 		account.close()
+		tD.close()
+	}
+})
+
+test('REGISTER : error if account exists', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	let ID = 0
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		ID = await account.getID('account')
+		const obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, `account with techID "${ID}" already exists`)
+	} finally {
+		account.close()
+		tD.close()
 	}
 })
 
@@ -90,6 +113,7 @@ test('REGISTER : success', async test => {
 		test.fail('error thrown')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
 
@@ -110,6 +134,7 @@ test('GET DETAILS : error if blank techID', async test => {
 		test.is(err.message, 'missing field', 'incorrect error message')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
 
@@ -125,6 +150,7 @@ test('GET DETAILS : error if techID has no associated details', async test => {
 		test.is(err.message, 'no results / details for technician with ID "1"')
 	} finally {
 		account.close()
+		tD.close()
 	}
 })
 
@@ -150,5 +176,200 @@ test('GET DETAILS : success ', async test => {
 		test.fail(err.message)
 	} finally {
 		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : error if blank techID', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		await tD.updateAll('', 456447895, obj, '16 Barneby Road')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : error if blank phone number', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		await tD.updateAll(ID, '', obj, '16 Barneby Road')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : error if blank types', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		await tD.updateAll(ID, 456447895, '', '16 Barneby Road')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : error if blank address', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		await tD.updateAll(ID, 456447895, obj, '')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : error if account does not exist', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	const ID = 0
+	try {
+		const obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		await tD.updateAll(1, 456447895, obj, '16 Barneby Lane')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'no records matching technician ID "1"')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE ALL : success', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		const value = await tD.updateAll(ID, 456447895, obj, '16 Barneby Lane')
+		test.is(value, true)
+	} catch(err) {
+		test.fail('error thrown')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE VALUE : error if blank value', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		const value = await tD.updateValue(ID, '', 'phoneNumber')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE VALUE : error if blank attribute', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		const value = await tD.updateValue(ID, 456447895, '')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE VALUE : error if account does not exist', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	const ID = 0
+	try {
+		await tD.updateValue(1, 456447895, 'phoneNumber')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'no records matching technician ID "1"', 'incorrect error message')
+	} finally {
+		account.close()
+		tD.close()
+	}
+})
+
+test('UPDATE VALUE : success', async test => {
+	test.plan(1)
+	const account = await new Accounts()
+	const tD = await new TechDetails()
+	try {
+		await account.register('account', 'password', 'technician', 'doej@gmail.com')
+		const ID = await account.getID('account')
+		let obj={fridge: true,microwave: true,freezer: true,boiler: true,washing_machine: true,tumble_dryer: true}
+		obj = JSON.stringify(obj)
+		await tD.register(ID, 456447895, obj, '16 Barneby Lane')
+		const value = await tD.updateValue(ID, 456447895, 'phoneNumber')
+		test.is(value, true)
+	} catch(err) {
+		console.log(err)
+		test.fail('error thrown')
+	} finally {
+		account.close()
+		tD.close()
 	}
 })
